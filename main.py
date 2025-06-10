1a41f77f10999c646947e8ac89f41dd2c681036b
Taha Serhan Yetiş <e210507055@stud.tau.edu.tr>
2025-06-10 13:38:42 +0300

    Added deletion also osme experimentation

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import sys
 import sqlite3
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLineEdit, QPushButton,
-    QVBoxLayout, QListWidget, QComboBox, QLabel, QHBoxLayout
+    QVBoxLayout, QListWidget, QComboBox, QLabel, QHBoxLayout, QCompleter
 )
 from PyQt5.QtGui import QIntValidator
 
 class GenelArayuz(QWidget):
     def __init__(self):
-        super().__init__()
+        super().__init__() #Eğer super().__init__() yazılmazsa, QWidget sınıfının başlatıcısı (constructor) çağrılmaz
         self.veritabani_olustur()
         self.setWindowTitle("Ders Programı Oluşturucu")
-        self.setGeometry(100, 100, 400, 350)
+        self.setGeometry(100, 100, 400, 400)
 
         # Giriş alanları
         self.ders_input = QLineEdit()
@@ -19,6 +19,7 @@ class GenelArayuz(QWidget):
 
         self.hoca_input = QLineEdit()
         self.hoca_input.setPlaceholderText("Hoca Adı")
+        self.hoca_input.setCompleter(QCompleter(self.hocalari_getir()))
 
         self.gun_input = QComboBox()
         self.gun_input.addItems(["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"])
@@ -28,12 +29,12 @@ class GenelArayuz(QWidget):
         self.saat_baslangic = QLineEdit()
         self.saat_baslangic.setPlaceholderText("Başlangıç (hhmm)")
         self.saat_baslangic.setMaxLength(5)
-        self.saat_baslangic.setValidator(QIntValidator(0, 2359))
+        self.saat_baslangic.setValidator(QIntValidator(0, 12359))
 
         self.saat_bitis = QLineEdit()
         self.saat_bitis.setPlaceholderText("Bitiş (hhmm)")
         self.saat_bitis.setMaxLength(5)
-        self.saat_bitis.setValidator(QIntValidator(0, 2359))
+        self.saat_bitis.setValidator(QIntValidator(0, 12359))
 
         saat_layout.addWidget(self.saat_baslangic)
         saat_layout.addWidget(QLabel(" - "))
@@ -42,6 +43,9 @@ class GenelArayuz(QWidget):
         # Buton ve liste
         self.ekle_button = QPushButton("Dersi Ekle")
         self.ekle_button.clicked.connect(self.ders_ekle)
+
+        self.sil_button = QPushButton("Seçili Dersi Sil")
+        self.sil_button.clicked.connect(self.ders_sil)
 
         self.ders_listesi = QListWidget()
 
@@ -52,6 +56,7 @@ class GenelArayuz(QWidget):
         layout.addWidget(self.gun_input)
         layout.addLayout(saat_layout)
         layout.addWidget(self.ekle_button)
+        layout.addWidget(self.sil_button)
         layout.addWidget(self.ders_listesi)
 
         self.setLayout(layout)
@@ -82,6 +87,26 @@ class GenelArayuz(QWidget):
         self.hoca_input.clear()
         self.saat_baslangic.clear()
         self.saat_bitis.clear()
+
+        self.hoca_input.setCompleter(QCompleter(self.hocalari_getir()))
+
+    def ders_sil(self):
+        secili = self.ders_listesi.currentItem()
+        if secili:
+            metin = secili.text()
+            try:
+                ders, hoca, kalan = metin.split(" - ")
+                gun, saat = kalan.split(" ")
+                self.cursor.execute("DELETE FROM dersler WHERE ders=? AND hoca=? AND gun=? AND saat=?",
+                                    (ders, hoca, gun, saat))
+                self.conn.commit()
+                self.ders_listesi.takeItem(self.ders_listesi.row(secili))
+            except:
+                pass
+
+    def hocalari_getir(self):
+        self.cursor.execute("SELECT DISTINCT hoca FROM dersler")
+        return [satir[0] for satir in self.cursor.fetchall() if satir[0]]
 
     def veritabani_olustur(self):
         self.conn = sqlite3.connect("dersler.db")
